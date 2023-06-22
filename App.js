8aaf467679a505ead4916f6eaf14ccb1d83534d0
Jaime Mitsuru Hirai Filho <jaimemitsuru@gmail.com>
2023-06-22 09:56:33 -0300

    Comentário para teste
    Commit de teste

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer, StackAction } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+//comentário para teste
 const Stack = createNativeStackNavigator()
 export default function App() {
   return (
